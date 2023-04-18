c5d88cf2536518680e50626f3cbe0f6688617a76
Teranur <60581325+Teranur@users.noreply.github.com>
2023-04-18 07:01:55 +0100

    Update config.py

--- config.py
+++ config.py
@@ -9,4 +9,4 @@ class DefaultConfig:
 
     PORT = 3978
     APP_ID = os.environ.get("c292f96e-db6f-4937-b0e8-069df21108cc")
-    APP_PASSWORD = os.environ.get("OPj8Q~_.QzBMEpl.TFyPZTj9rWS9F4vN2u6lxaB2")
+    APP_PASSWORD = os.environ.get("8496f60a-b17b-46c8-97fe-04603bd8efa6")
