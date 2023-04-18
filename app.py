1e503e05c40bcedfc397e782b03f87e1ec5bc52a
Teranur <60581325+Teranur@users.noreply.github.com>
2023-04-18 10:39:54 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,91 +1,59 @@
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License.
-
-import sys
-import traceback
-from datetime import datetime
-from aiohttp import web
-from aiohttp.web import Request, Response, json_response
 from botbuilder.core import (
-    BotFrameworkAdapterSettings,
-    TurnContext,
-    BotFrameworkAdapter,
+    ConversationState,
+    MemoryStorage,
+    UserState,
 )
-
-from botbuilder.core.integration import aiohttp_error_middleware
-from botbuilder.schema import Activity, ActivityTypes
-from botbuilder.core import MemoryStorage, UserState, ConversationState
+from botbuilder.schema import Activity
+from botbuilder.integration.aiohttp import BotFrameworkHttpAdapter
+from aiohttp import web
+from botbuilder.core import BotFrameworkAdapterSettings
+from botbuilder.integration.aiohttp import BotFrameworkHttpAdapter
+from bot2 import SearchBot
 from bot import CovUniChatBot
-from config import DefaultConfig
 
-CONFIG = DefaultConfig()
+# Load settings
+app_id = "c292f96e-db6f-4937-b0e8-069df21108cc"
+app_password = "8496f60a-b17b-46c8-97fe-04603bd8efa6"
 
-# Create adapter.
-# See https://aka.ms/about-bot-adapter to learn more about how bots work.
-SETTINGS = BotFrameworkAdapterSettings(CONFIG.APP_ID, CONFIG.APP_PASSWORD)
-ADAPTER = BotFrameworkAdapter(SETTINGS)
+# Create adapter
+settings = BotFrameworkAdapterSettings(app_id, app_password)
+adapter = BotFrameworkHttpAdapter(settings)
 
+# Create memory storage
+memory = MemoryStorage()
 
-# Catch-all for errors.
-async def on_error(context: TurnContext, error: Exception):
-    # This check writes out errors to console log .vs. app insights.
-    # NOTE: In production environment, you should consider logging this to Azure
-    #       application insights.
-    print(f"\n [on_turn_error] unhandled error: {error}", file=sys.stderr)
-    traceback.print_exc()
+# Create conversation state
+conversation_state = ConversationState(memory)
 
-    # Send a message to the user
-    await context.send_activity("The bot encountered an error or bug.")
-    await context.send_activity(
-        "To continue to run this bot, please fix the bot source code."
-    )
-    # Send a trace activity if we're talking to the Bot Framework Emulator
-    if context.activity.channel_id == "emulator":
-        # Create a trace activity that contains the error object
-        trace_activity = Activity(
-            label="TurnError",
-            name="on_turn_error Trace",
-            timestamp=datetime.utcnow(),
-            type=ActivityTypes.trace,
-            value=f"{error}",
-            value_type="https://www.botframework.com/schemas/error",
-        )
-        # Send a trace activity, which will be displayed in Bot Framework Emulator
-        await context.send_activity(trace_activity)
+# Create user state
+user_state = UserState(memory)
+
+# Create bot
+#BOT = CovUniChatBot()
+Bot = SearchBot(conversation_state, user_state)
 
 
-ADAPTER.on_turn_error = on_error
-
-# Create the Bot
-
-BOT = CovUniChatBot()
-
-
-# Listen for incoming requests on /api/messages
-async def messages(req: Request) -> Response:
-    # Main bot message handler.
+async def messages(req: web.Request) -> web.Response:
     if "application/json" in req.headers["Content-Type"]:
         body = await req.json()
     else:
-        return Response(status=415)
+        return web.Response(status=415)
 
     activity = Activity().deserialize(body)
     auth_header = req.headers["Authorization"] if "Authorization" in req.headers else ""
-
     try:
-        response = await ADAPTER.process_activity(activity, auth_header, BOT.on_turn)
+        response = await adapter.process_activity(activity, auth_header, BOT.on_turn)
         if response:
-            return json_response(data=response.body, status=response.status)
-        return Response(status=201)
+            return web.json_response(data=response.body, status=response.status)
+        return web.Response(status=201)
     except Exception as exception:
         raise exception
 
-
-APP = web.Application(middlewares=[aiohttp_error_middleware])
-APP.router.add_post("/api/messages", messages)
+app = web.Application()
+app.router.add_post("/api/messages", messages)
 
 if __name__ == "__main__":
     try:
-        web.run_app(APP, host="localhost", port=CONFIG.PORT)
-    except Exception as error:
-        raise error
+        web.run_app(app, host="localhost", port=3978)
+    except Exception as exception:
+        raise exception
